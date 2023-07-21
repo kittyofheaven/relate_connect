88c7cd0e9016a060c19bcddf7dbc326b126d723e
Hazel Handrata <hazelhandrata@Hazels-MacBook.local>
2023-07-22 05:11:13 +0700

    quiz selesai man omg sisa frontendnya

---
+++ app/quiz/data.js
@@ -0,0 +1,96 @@
+export const quiz = {
+    totalQuestions: 18,
+    questions: [
+        {
+            id: 1,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 2,
+            question: "When I'm on a date I might ask myself, Is this person up to my standards?",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 3,
+            question: "I'll be ready to date when I improve myself (for example, lose weight or feel more financially stable)",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 4,
+            question: "I'd prefer if my partner and I had a romantic 'how we met' story.",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 5,
+            question: "I usually read reviews before I make a significant purchase.",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 6,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 7,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 8,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 9,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 10,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 11,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 12,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 13,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 14,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 15,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 16,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 17,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+        {
+            id: 18,
+            question: "I don't want to go on a second date with someone if I don't feel the spark when we meet",
+            answers: ['Very unlike me',  'Somewhat like me', 'Very like me']
+        },
+    ]
+
+}
---
+++ app/quiz/page.jsx
@@ -0,0 +1,176 @@
+'use client'
+import React, {useState} from 'react'
+import db from '../../utils/dbconfig'
+import { useSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+import {
+    addDoc,
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    limit,
+    onSnapshot,
+    or,
+    orderBy,
+    query,
+    setDoc,
+    updateDoc,
+    where
+} from 'firebase/firestore'
+
+import { quiz } from './data'
+import { useRouter } from 'next/navigation'
+
+
+export default function page({ params }) {
+
+    const router = useRouter()
+
+    const {data: session} = useSession({
+        required: true,
+        onUnauthenticated: () => {
+            redirect('/profile')
+        }
+    })
+
+    
+
+    const [activeQuestion, setActiveQuestion] = useState(0)
+    const [selectedAnswer, setSelectedAnswer] = useState('')
+    const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null)
+    const [showResult, setShowResult] = useState(false)
+    const [checked, setChecked] = useState(false)
+
+    const [result, setResult] = useState({
+        romanticizerScore: 0,
+        maximizerScore: 0,
+        hesitaterScore: 0
+    })
+
+    const {questions} = quiz
+    const {question, answers} = questions[activeQuestion]
+
+
+
+    // console.log(quiz)
+    // console.log(answers)
+
+
+    // if params.id is not in question array, redirect to 404
+    
+    const romanticizer =[0,3,6,9,12,15]
+    const maximizer =[1,4,7,10,13,16]
+    const hesitater =[2,5,8,11,14,17]
+
+
+    
+    if(session && session.user){
+
+        function onAnswerSelected(answer, index) {
+    
+            setChecked(true)
+    
+            const score = index + 1
+            // console.log(score)
+    
+            if(romanticizer.includes(activeQuestion)) {
+                
+                setResult({...result, romanticizerScore: result.romanticizerScore + score})
+    
+            } else if(maximizer.includes(activeQuestion)) {
+    
+                setResult({...result, maximizerScore: result.maximizerScore + score})
+                
+            } else if(hesitater.includes(activeQuestion)) {
+                
+                setResult({...result, hesitaterScore: result.hesitaterScore + score})
+    
+            }
+    
+            if (activeQuestion === questions.length - 1) {
+                
+            } else {
+                setActiveQuestion(activeQuestion + 1)
+            }
+    
+        }
+    
+        async function addResultToDatabase(result) {
+
+            const email = session.user.email
+
+            const user_ref = collection(db, 'users') //collection buat ngambil collection dari firestore
+            const q = query(user_ref, where('email', '==', email), limit(1))
+            const querySnapshot = await getDocs(q) //getDocs buat ngambil data dari query
+            const user_id = querySnapshot.docs[0].id //docs buat ngambil data dari querySnapshot
+
+            const user_reference = doc(db, `users/${user_id}`)
+            await updateDoc(user_reference, {categories : result})
+
+            // console.log(docRef)
+    
+            // const res = await docRef.set({
+            //     categories : result
+            // }, { merge: true });
+    
+        }
+    
+        function upResult() {
+            
+            const resultArray = [result.romanticizerScore, result.maximizerScore, result.hesitaterScore]
+            const maxResult = resultArray.indexOf(Math.max(...resultArray))
+    
+            let fixResult 
+    
+            if (maxResult === 0) {
+                fixResult = 'romanticizer'
+            }
+            else if (maxResult === 1) {
+                fixResult = 'maximizer'
+            }
+            else if (maxResult === 2) {
+                fixResult = 'hesitater'
+            }
+    
+    
+            //! add to database
+            console.log(fixResult)
+            addResultToDatabase(fixResult)
+
+            router.push('/')
+
+            
+            
+        }
+
+
+
+        return (
+            <div>
+                <div className='justify-center text-4xl text-center mt-10'>{activeQuestion + 1}/{questions.length}</div>
+                <div className='justify-center text-4xl text-center mt-10'>{questions[activeQuestion].question}</div>
+                <div className='text-center items-center justify-between mt-4'>
+    
+                    {answers.map((answer, index) => {
+                        return (
+                            <button 
+                            key={index} 
+                            onClick={() => onAnswerSelected(answer, index)}
+                            className='mx-4 border-black border-b-4 border-r-4 bg-green-500 rounded-lg h-20 w-30 px-7'>{answer}</button>
+                        )
+                    })}
+                </div>
+                
+                {activeQuestion === questions.length - 1 ? (
+    
+                    <button onClick={() => upResult()}>Finish</button>
+    
+                ) : (<></>)}
+    
+            </div>
+        )
+    }
+
+    
+}
