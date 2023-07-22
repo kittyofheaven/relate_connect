d4fb090dde308977237ea9598ef213b95e7b7921
Hazel Handrata <hazelhandrata@Hazels-MacBook.local>
2023-07-22 11:56:48 +0700

    page jsx app dashboard dynamic tips

--- app/page.jsx
+++ app/page.jsx
@@ -1,129 +1,186 @@
+'use client'
+
+import React, {useState} from 'react'
 import Link from 'next/link'
-
-import { getServerSession } from 'next-auth/next'
-import { authOptions } from './api/auth/[...nextauth]/route'
 import { redirect } from 'next/navigation'
+import { useSession } from 'next-auth/react'
 
-export default async function Home() {
+import {
+  addDoc,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  limit,
+  onSnapshot,
+  or,
+  orderBy,
+  query,
+  setDoc,
+  updateDoc,
+  where
+} from 'firebase/firestore'
 
-  const session = await getServerSession(authOptions)
+import db from '../utils/dbconfig'
 
-  if (!session) {
-    redirect('/profile')
+import { tipsData } from './tipsData'
+
+export default function Home() {
+
+  const {data: session} = useSession({
+    required: true,
+    onUnauthenticated: () => {
+        redirect('/profile')
+      }
+  })
+
+  const [userUniqueId, setUserUniqueId] = useState('')
+  const [partnerUniqueId, setPartnerUniqueId] = useState('')
+  const [categories, setCategories] = useState('')
+
+
+  
+  
+  if(session && session.user){
+
+    async function getPartnerUniqueId(id){
+      const user_1 = doc(db, `users/${id}`)
+      const user_1_data = await getDoc(user_1)
+      setPartnerUniqueId(user_1_data.data().partner)
+      return(partnerUniqueId)
+      // console.log(user_1) //bakal return promise doang, bukan data nya
+    }
+  
+    async function getCategories(id){
+        const user_1 = doc(db, `users/${id}`)
+        const user_1_data = await getDoc(user_1)
+        setCategories(user_1_data.data().categories)
+        return(categories)
+    }
+  
+    async function userUniqueIdFunc() {
+        
+        const email = session.user.email
+  
+        const user_ref = collection(db, 'users') //collection buat ngambil collection dari firestore
+        const q = query(user_ref, where('email', '==', email), limit(1))
+        const querySnapshot = await getDocs(q) //getDocs buat ngambil data dari query
+        const user_id = querySnapshot.docs[0].id //docs buat ngambil data dari querySnapshot
+  
+        // console.log(user_id)
+  
+        getPartnerUniqueId(user_id)
+        getCategories(user_id)
+        setUserUniqueId(user_id)
+  
+    }
+  
+    userUniqueIdFunc()
+
+    return (
+  
+      <div>
+        <div className='text-center'>
+          <div className='text-5xl my-5'>
+            <h5>Hello There, {session.user.name} !</h5>
+          </div>
+          <div className='my-4'>
+            <p>You are categorized as, </p>
+          </div>
+        </div>
+        {/* //CARD 2 */}
+        <div className='bg-white rounded-xl shadow-lg mx-24 mb-8'>
+          <div className='p-5 flex flex-col'>
+  
+            {/* TITLE START */}
+            <h5 className='text-2xl font-medium mt-3 border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg  hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>The {categories.toUpperCase()}</h5>
+            {/* TITLE END */}
+  
+            {/* DESCRIPTION START */}
+            <p className='text-lg mt-3'>
+              {categories ? tipsData[categories].description : 'Loading...'}
+            </p>
+
+            <ol type='1'>
+              {categories ? tipsData[categories].tips.map((tip) => {
+                return (
+                  <li className='text-lg mt-3'>● {tip}</li>  //! ini ada emot symbol ● barangkali klo lempar error ini kemungkinan bisa jadi penyebab
+                )
+              }) : 'Loading...'}
+            </ol>
+            {/* DESCRIPTION END */}
+
+  
+          </div>
+        </div>
+  
+  
+        <div className=' flex min-h-screen min-w-full container items-center justify-between -mt-14'>
+          {/* //GRID START */}
+          <div className=' grid grid-cols-4 gap-9 '>
+  
+            {/* //CARD 2 */}
+            <div className='bg-white rounded-xl shadow-lg'>
+              <div className='p-5 flex flex-col'>
+                <div className='rounded-xl overflow-hidden  border-b-4 border-r-4 border-black'>
+                  <img src='https://hips.hearstapps.com/hmg-prod/images/close-up-of-unrecognizable-woman-text-messaging-on-royalty-free-image-1582276818.jpg?crop=0.668xw:1.00xh;0.0442xw,0&resize=1200:*' alt='' />
+                </div>
+                <h5 className='text-2xl font-medium mt-3 '>AI Text Rater</h5>
+                <p className='text-lg mt-3'>Practice with an AI on how you should text
+                  your partner, the AI will then rate your responses and in that way, you will be able to know how you should and shouldn't  text your partner. </p>
+                <a href='#' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
+              </div>
+            </div>
+  
+            {/* //CARD 1 */}
+            <div className='bg-white rounded-xl shadow-lg'>
+              <div className='p-5 flex flex-col'>
+                <div className='rounded-xl overflow-hidden border-black border-b-4 border-r-4'>
+                  <img src='https://commit30.com/cdn/shop/products/WeeklyDayPlannerInside_1280x.jpg?v=1688748909' alt='' />
+                </div>
+                <h5 className='text-2xl font-medium mt-3 '>Auto Date Planner</h5>
+                <p className='text-lg mt-3'>Ask an AI to plan a date for you since you're too busy thinking about everything else! You may be busy but a date is always necessary if you still want a relationship. </p>
+                <a href='#' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
+              </div>
+            </div>
+  
+            {/* //CARD 3 */}
+            <div className='bg-white rounded-xl shadow-lg'>
+              <div className='p-5 flex flex-col'>
+                <div className='rounded-xl overflow-hidden border-black border-b-4 border-r-4'>
+                  <img src='https://hips.hearstapps.com/hmg-prod/images/img-20200415-110729-1586974804.jpg?crop=0.752xw:1.00xh;0.119xw,0&resize=1200:*' alt='' />
+                </div>
+                <h5 className='text-2xl font-medium mt-3 '>His/Her Journal</h5>
+                <p className='text-lg mt-3'> Sometimes it's hard being honest, but this time it doesn't have to. The journal allows you to write about how your relationship has been going & you could read it again and reflect on it. </p>
+                <a href='#' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
+              </div>
+            </div>
+  
+            {/* //CARD 4 */}
+            <div className='bg-white rounded-xl shadow-lg'>
+              <div className='p-5 flex flex-col'>
+                <div className='rounded-xl overflow-hidden border-black border-b-4 border-r-4'>
+                  <img src='https://www.hobbycraft.co.uk/on/demandware.static/-/Sites-hobbycraft-uk-master/default/dw4a34cd26/images/Ideas/papercraft/main/ideas_main_how-to-make-a-calendar-scrapbook.jpg' alt='' />
+                </div>
+                <h5 className='text-2xl font-medium mt-3 '>My Calendar</h5>
+                <p className='text-lg mt-3'>Keeping track of dates are mundane, But through the calender it would allow you to keep track of special events with you and your partner. For e.g, Birthdays, Anniversaries, etc. </p>
+                <a href='#' className=' border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
+              </div>
+            </div>
+          </div>
+          {/* //GRID END */}
+        </div>
+  
+  
+        <div className='flex flex-col items-center justify-center'>
+          <Link href='/quiz' className=' border-b-4 border-r-4 mb-5 -mt-6 text-center bg-pink-300 text-pink-700 py-2 px-5 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Retake the tendency Quiz ✨</Link>
+        </div>
+      </div>
+  
+  
+  
+    )
+
   }
 
-
-  return (
-
-    <div>
-      <div className='text-center'>
-        <div className='text-5xl my-5'>
-          <h5>Hello There, "Username" !</h5>
-        </div>
-        <div className='my-4'>
-          <p>You are categorized as, </p>
-        </div>
-      </div>
-      {/* //CARD 2 */}
-      <div className='bg-white rounded-xl shadow-lg mx-24 mb-8'>
-        <div className='p-5 flex flex-col'>
-
-          {/* TITLE START */}
-          <h5 className='text-2xl font-medium mt-3 border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg  hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>The Romanticizer</h5>
-          {/* TITLE END */}
-
-          {/* DESCRIPTION START */}
-          <p className='text-lg mt-3'>
-            You want the soul mate, the happily ever after—the whole fairy tale. You
-            love love. You believe you are single because you haven't met the right
-            person yet. Your motto: It'll happen when it's meant to happen.</p>
-          <p>
-            1. Many people suffer from dating blind spots—patterns of behavior that
-            hold them back from finding love, but which they can't identify on their
-            own.<br />
-            2. I've categorized the most common blind spots into a framework called
-            The Three Dating Tendencies. Each group struggles with unrealistic
-            expectations.<br />
-            The Romanticizer has unrealistic expectations of relationships.
-            They want the soul mate, the happily ever after—the whole fairy
-            tale. <br />
-            The Maximizer has unrealistic expectations of their partner. They
-            love to explore their options and want to feel absolutely confident
-            they're making the right decision. <br />
-            The Hesitater has unrealistic expectations of themselves. They feel
-            like they're not ready to date.<br />
-            3. Understanding your dating tendency helps you discover what's holding
-            you back and how you can overcome these blind spots.
-          </p>
-          {/* DESCRIPTION END */}
-
-        </div>
-      </div>
-
-
-      <div className=' flex min-h-screen min-w-full container items-center justify-between -mt-14'>
-        {/* //GRID START */}
-        <div className=' grid grid-cols-4 gap-9 '>
-
-          {/* //CARD 2 */}
-          <div className='bg-white rounded-xl shadow-lg'>
-            <div className='p-5 flex flex-col'>
-              <div className='rounded-xl overflow-hidden  border-b-4 border-r-4 border-black'>
-                <img src='https://hips.hearstapps.com/hmg-prod/images/close-up-of-unrecognizable-woman-text-messaging-on-royalty-free-image-1582276818.jpg?crop=0.668xw:1.00xh;0.0442xw,0&resize=1200:*' alt='' />
-              </div>
-              <h5 className='text-2xl font-medium mt-3 '>AI Text Rater</h5>
-              <p className='text-lg mt-3'>Practice with an AI on how you should text
-                your partner, the AI will then rate your responses and in that way, you will be able to know how you should and shouldn't  text your partner. </p>
-              <a href='#' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
-            </div>
-          </div>
-
-          {/* //CARD 1 */}
-          <div className='bg-white rounded-xl shadow-lg'>
-            <div className='p-5 flex flex-col'>
-              <div className='rounded-xl overflow-hidden border-black border-b-4 border-r-4'>
-                <img src='https://commit30.com/cdn/shop/products/WeeklyDayPlannerInside_1280x.jpg?v=1688748909' alt='' />
-              </div>
-              <h5 className='text-2xl font-medium mt-3 '>Auto Date Planner</h5>
-              <p className='text-lg mt-3'>Ask an AI to plan a date for you since you're too busy thinking about everything else! You may be busy but a date is always necessary if you still want a relationship. </p>
-              <a href='#' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
-            </div>
-          </div>
-
-          {/* //CARD 3 */}
-          <div className='bg-white rounded-xl shadow-lg'>
-            <div className='p-5 flex flex-col'>
-              <div className='rounded-xl overflow-hidden border-black border-b-4 border-r-4'>
-                <img src='https://hips.hearstapps.com/hmg-prod/images/img-20200415-110729-1586974804.jpg?crop=0.752xw:1.00xh;0.119xw,0&resize=1200:*' alt='' />
-              </div>
-              <h5 className='text-2xl font-medium mt-3 '>His/Her Journal</h5>
-              <p className='text-lg mt-3'> Sometimes it's hard being honest, but this time it doesn't have to. The journal allows you to write about how your relationship has been going & you could read it again and reflect on it. </p>
-              <a href='#' className='border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
-            </div>
-          </div>
-
-          {/* //CARD 4 */}
-          <div className='bg-white rounded-xl shadow-lg'>
-            <div className='p-5 flex flex-col'>
-              <div className='rounded-xl overflow-hidden border-black border-b-4 border-r-4'>
-                <img src='https://www.hobbycraft.co.uk/on/demandware.static/-/Sites-hobbycraft-uk-master/default/dw4a34cd26/images/Ideas/papercraft/main/ideas_main_how-to-make-a-calendar-scrapbook.jpg' alt='' />
-              </div>
-              <h5 className='text-2xl font-medium mt-3 '>My Calendar</h5>
-              <p className='text-lg mt-3'>Keeping track of dates are mundane, But through the calender it would allow you to keep track of special events with you and your partner. For e.g, Birthdays, Anniversaries, etc. </p>
-              <a href='#' className=' border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Explore</a>
-            </div>
-          </div>
-        </div>
-        {/* //GRID END */}
-      </div>
-
-
-      <div className='flex flex-col items-center justify-center'>
-        <Link href='/quiz' className=' border-b-4 border-r-4 mb-5 -mt-6 text-center bg-pink-300 text-pink-700 py-2 px-5 rounded-lg font-semibold mt-4 hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>Retake the tendency Quiz ✨</Link>
-      </div>
-    </div>
-
-
-
-  )
 }
---
+++ app/tipsData.js
@@ -0,0 +1,35 @@
+export const tipsData= {
+    romanticizer: {
+        categories: 'romanticizer',
+        description : "You want the soul mate, the happily ever after—the whole fairy tale. You love love. You believe you are single because you haven’t met the right person yet. Your motto: It’ll happen when it’s meant to happen.",
+        tips: [
+            "Our mindset matters! The ability to shift your mindset from soul mate to work-it-out beliefs could mean the difference between finding a life partner or not. People with soul mate beliefs reject promising partners because they don’t match their vision for what love should look and feel like. They think that love will just happen to them. They expect love to be effortless. If it’s not, they must be with the wrong person. People with a work-it-out mindset know that relationships take effort and that building a successful relationship is a process.", 
+            "Our belief in fate and fairy tales—caused in part by Disney movies,rom-coms, and social media—creates unrealistic expectations for finding and sustaining relationships. Remember, no one is perfect, including you. Even Prince Charming has morning breath.", 
+            "The Happily-Ever-After Fallacy is the mistaken idea that the hard work of love is finding someone. In reality, that’s only the beginning. Staying in love takes work, too. If you expect relationships to be easy, you’ll be caught off guard when they hit an inevitable rough patch. It’s time to embrace (and seek out) real love, scuffs and all!", 
+            "It’s time to embrace (and seek out) real love, scuffs and all!"
+        ]
+    },
+    maximizer: {
+        categories: 'maximizer',
+        description : "You love doing research, exploring all of your options, turning over everystone until you’re confident you’ve found the right one. You make decisions carefully. And you want to be 100 percent certain about something before you make your choice. Your motto: Why settle?",
+        tips: [
+            "Maximizers obsess over making the right decision. They want to explore every possible option before they make a choice. Even when they decide, they constantly wonder what they’re missing out on. Satisficers figure out what they want and stop looking once they’ve met their criteria. They don’t settle, they merely stop worrying what else is out there once they’ve made a decision.",
+            "Research shows that Satisficers tend to be happier, because in the end, satisfaction comes from how you feel about your decision, not the decision itself.",
+            "The current dating climate creates Maximizers out of many of us. No one ever seems good enough, and we wonder if we could be happier with someone else. Maximizing tendencies in relationships can lead to mental anguish, costly delays in decision-making, and missed opportunities.",
+            "Maximizers assume there is a right answer for whom to be with. And there’s not. We can apply lessons from the Secretary Problem to see that we likely already have enough dating experience to select a great partner. This knowledge can help us commit without worrying about what else is out there. The power of rationalization can also help us embrace our decisions."
+
+        ]
+    },
+    hesitater: {
+        categories: 'hesitater',
+        description : "You don’t think you’re ready for dating because you’re not the person you want to be yet. You hold yourself to a high standard. You want to feel completely ready before you start a new project; the same goes for dating. Your motto: I’ll wait until I’m a catch.",
+        tips: [
+            "Hesitaters delay dating because they don’t feel 100 percent ready yet and want to put their best foot forward. But no one ever feels 100 percent ready for anything. At a certain point, you just have to start.",
+            "Perfection is a lie. Everyone else is imperfect, too—even the person you’ll eventually end up with.",
+            "By waiting to date, Hesitaters miss out on a chance to develop their dating skills and figure out what type of person they want to be with."
+        ]
+    },
+    
+
+
+}
