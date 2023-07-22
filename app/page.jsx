2c850b84ba41cf458a6a64d3d31420f8ccf1dc41
Fathan Maulana Prasetya <fathan.maulana.prasetya@gmail.com>
2023-07-22 11:10:01 +0700

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -21,7 +21,42 @@ export default async function Home() {
           <h5>Hello There, "Username" !</h5>
         </div>
         <div className='my-4'>
-          <p>You are a "The Tendency", </p>
+          <p>You are categorized as, </p>
+        </div>
+      </div>
+      {/* //CARD 2 */}
+      <div className='bg-white rounded-xl shadow-lg mx-24 mb-8'>
+        <div className='p-5 flex flex-col'>
+
+          {/* TITLE START */}
+          <h5 className='text-2xl font-medium mt-3 border-b-4 border-r-4 text-center bg-pink-300 text-pink-700 py-2 rounded-lg  hover:bg-pink-500 focus:scale-95 transition-all duration-200 ease-out'>The Romanticizer</h5>
+          {/* TITLE END */}
+
+          {/* DESCRIPTION START */}
+          <p className='text-lg mt-3'>
+            You want the soul mate, the happily ever after—the whole fairy tale. You
+            love love. You believe you are single because you haven't met the right
+            person yet. Your motto: It'll happen when it's meant to happen.</p>
+          <p>
+            1. Many people suffer from dating blind spots—patterns of behavior that
+            hold them back from finding love, but which they can't identify on their
+            own.<br />
+            2. I've categorized the most common blind spots into a framework called
+            The Three Dating Tendencies. Each group struggles with unrealistic
+            expectations.<br />
+            The Romanticizer has unrealistic expectations of relationships.
+            They want the soul mate, the happily ever after—the whole fairy
+            tale. <br />
+            The Maximizer has unrealistic expectations of their partner. They
+            love to explore their options and want to feel absolutely confident
+            they're making the right decision. <br />
+            The Hesitater has unrealistic expectations of themselves. They feel
+            like they're not ready to date.<br />
+            3. Understanding your dating tendency helps you discover what's holding
+            you back and how you can overcome these blind spots.
+          </p>
+          {/* DESCRIPTION END */}
+
         </div>
       </div>
 
