070834b49203c5cf7187a90395dba30ee48249ed
Fathan Maulana Prasetya <fathan.maulana.prasetya@gmail.com>
2023-07-23 01:46:10 +0700

    Update page.jsx

--- app/dateplanner/page.jsx
+++ app/dateplanner/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 
@@ -12,44 +12,50 @@ export default function page() {
 
     const [AiResult, setAiResult] = useState('')
 
-    async function getAiResult(place, time){
+    async function getAiResult(place, time) {
         const cohere = require('cohere-ai');
         cohere.init('DWFkWRSVIoP2s9xHZE6i2JUjVRBKTaOAHx21u1PB'); // This is your trial API key
         (async () => {
-        const response = await cohere.generate({
-            model: '3ec3f15a-0b0a-479c-a479-bbaea8077153-ft',
-            prompt: `make a summarized date plan at the vicinity of ${place} at ${time}`,
-            max_tokens: 1564,
-            temperature: 1,
-            k: 0,
-            stop_sequences: [],
-            return_likelihoods: 'NONE'
-        });
-        setAiResult(`${response.body.generations[0].text}`);
+            const response = await cohere.generate({
+                model: '3ec3f15a-0b0a-479c-a479-bbaea8077153-ft',
+                prompt: `make a summarized date plan at the vicinity of ${place} at ${time}`,
+                max_tokens: 1564,
+                temperature: 1,
+                k: 0,
+                stop_sequences: [],
+                return_likelihoods: 'NONE'
+            });
+            setAiResult(`${response.body.generations[0].text}`);
         })();
     }
-    
+
     // getAiResult()
-    
+
     useEffect(() => {
         // console.log(AiResult)
     }, [AiResult])
 
 
-    function handleForm(e){
+    function handleForm(e) {
         e.preventDefault()
         getAiResult(e.target[0].value, e.target[1].value)
     }
 
 
-return (
-    <div>
-        <form onSubmit={handleForm}>
-            <input type="text" placeholder='place ? ex: Tunjungan'/>
-            <input type="text" placeholder='time ? ex: 4pm' />
-            <button type="submit">Create!</button>
-        </form>
-        <div>{AiResult}</div>
-    </div>
-)
+    return (
+        <div>
+            <form onSubmit={handleForm}>
+                <div className=' flex gap-9 items-center justify-center mt-5'>
+                    <input className='px-8 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-lg hover:shadow-sm' type="text" placeholder='place ? ex: Tunjungan' />
+                    <input className='px-8 py-2 font-medium border border-b-4 border-r-4 border-black rounded-lg shadow-lg hover:shadow-sm' type="text" placeholder='time ? ex: 4pm' />
+                    <button className='px-8 py-2 bg-pink-500 flex items-center p4 border-b-4 border-r-4 border-black rounded-md shadow-lg hover:bg-pink-200 ' type="submit">Create!</button>
+                </div>
+            </form>
+            {AiResult === '' ? (<></>) : (
+                <div className='bg-white rounded-lg py-4 border-b-4 border-r-4 p-8 text-md text-center mt-8 mx-80'>
+                    {AiResult}
+                </div>
+            )}
+        </div>
+    )
 }
