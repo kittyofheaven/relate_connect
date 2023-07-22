ebea38ef8e0988735d2af84c2d7309661dadc395
Hazel Handrata <hazelhandrata@Hazels-MacBook.local>
2023-07-22 13:56:57 +0700

    signin button selesai bro omg

--- app/components/signinbutton.jsx
+++ app/components/signinbutton.jsx
@@ -6,14 +6,32 @@ import { BiPaperPlane, BiUser } from "react-icons/bi";
 import { MdOutlineNotificationsNone } from "react-icons/md";
 import { useState, useEffect } from "react";
 
+import db from '../../utils/dbconfig'
+import {
+    addDoc,
+    collection,
+    doc,
+    getDoc,
+    getDocs,
+    limit,
+    onSnapshot,
+    or,
+    orderBy,
+    query,
+    setDoc,
+    updateDoc,
+    where
+} from 'firebase/firestore'
+
 export default function SigninButton() {
     const { data: session } = useSession(); //harus data variabelnya habis itu datanya direname jadi session pake : session
+    const [profpic, setProfpic] = useState('https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png')
+
 
 
     if (session && session.user) {
 
         // const email = session.user.email
-        // const [imageLink, setImageLink] = useState('')
 
         // useEffect({
 
@@ -21,7 +39,20 @@ export default function SigninButton() {
 
         // },[])  //! where the problem is
 
+        async function profPicsFunc() {
 
+            const email = session.user.email
+
+            const user_ref = collection(db, 'users') //collection buat ngambil collection dari firestore
+            const q = query(user_ref, where('email', '==', email), limit(1))
+            const querySnapshot = await getDocs(q) //getDocs buat ngambil data dari query
+            // return user image link
+            const userImageLink = await querySnapshot.docs[0].data().image //docs buat ngambil data dari querySnapshot
+
+            setProfpic(userImageLink)
+        }
+
+        profPicsFunc()
 
 
         return (
@@ -37,7 +68,7 @@ export default function SigninButton() {
                 {/* PROFPIC START */}
                 <Link href="/profile" className="flex gap-5 mr-4">
                     <img
-                        src="https://pbs.twimg.com/profile_images/1663819132234784770/2A7NJSJg_400x400.jpg"
+                        src={profpic}
                         className="w-10 h-10 rounded-full border-b-4 border-r-4 border-black"
                     />
                 </Link>
